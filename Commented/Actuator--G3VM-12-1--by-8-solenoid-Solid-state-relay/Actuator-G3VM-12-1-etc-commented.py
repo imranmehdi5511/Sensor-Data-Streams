db6d25c510e6c45ec7a94e70e84451aa418ebf02
imranmehdi5511 <74496936+imranmehdi5511@users.noreply.github.com>
2023-09-15 14:48:56 +0500

    Update Actuator-G3VM-12-1-etc-commented.py

--- Commented/Actuator--G3VM-12-1--by-8-solenoid-Solid-state-relay/Actuator-G3VM-12-1-etc-commented.py
+++ Commented/Actuator--G3VM-12-1--by-8-solenoid-Solid-state-relay/Actuator-G3VM-12-1-etc-commented.py
@@ -1,6 +1,7 @@
+#First of All import the required modules needed for the script. In this case the required module is time, and module numpy as np.
 import numpy as np
 import time
-
+#We used the Following values for the virtual sensor script. The maximum and minimum possible values of Frequency can be as low as 0.1 and as high as 1000. We arbitrarily ran the script for data rate value of 10 but the value can be changed as well. The data rate is basically same as the frequency. 
 # Actuator characteristics
 data_rate = 10  # Data rate in Hz
 baud_rate = 9600  # Baud rate in bits per second
