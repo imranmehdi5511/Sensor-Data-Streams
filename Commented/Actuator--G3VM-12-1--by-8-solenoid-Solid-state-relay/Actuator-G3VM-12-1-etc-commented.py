4833506ed3c179a28d741b4d0060aaa553c7d9e0
imranmehdi5511 <74496936+imranmehdi5511@users.noreply.github.com>
2023-09-15 15:39:12 +0500

    Update Actuator-G3VM-12-1-etc-commented.py

--- Commented/Actuator--G3VM-12-1--by-8-solenoid-Solid-state-relay/Actuator-G3VM-12-1-etc-commented.py
+++ Commented/Actuator--G3VM-12-1--by-8-solenoid-Solid-state-relay/Actuator-G3VM-12-1-etc-commented.py
@@ -15,7 +15,7 @@ frequency_response_high = 1000  # Frequency response high limit in Hz
 time_step = 1 / data_rate
 
 # Virtual actuator simulation
-with open('actuator_data.txt', 'a') as file:
+with open('actuator_solenoid_solid_state_relay_data.txt', 'a') as file:
     while True:
         # Generate current time stamp
         current_time = time.time()
@@ -24,7 +24,7 @@ with open('actuator_data.txt', 'a') as file:
         actuator_state = "Open" if np.random.rand() < 0.5 else "Closed"
 
         # Print and save actuator state
-        data_line = f"Actuator Solenoid Solid State Relay:- Timestamp: {current_time:.6f},Actuator State: {actuator_state}\n"
+        data_line = f"Actuator Solenoid Solid State Relay: Timestamp: {current_time:.6f},Actuator State: {actuator_state}\n"
         print(data_line.strip())  # Print without newline
         file.write(data_line)
 
